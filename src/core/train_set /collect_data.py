87dc69149de9810c11fb58e5e868c042466c402e
mborodada <mboroda@lytvynov-production.com>
2025-02-04 22:48:55 +0200

    initial files

---
+++ src/core/train_set /collect_data.py
@@ -0,0 +1,14 @@
+# todo:
+#  1) find viable and comprehensive data sources;
+#  2) preprocess data for further use;
+#  3) apply ETL approach
+
+
+class CollectData:
+
+    def __init__(self):
+        ...
+
+    async def web_sources(self):
+        ...
+
